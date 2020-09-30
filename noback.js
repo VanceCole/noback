8cf63b5256066b2be5808d7ed94ca5d726990424
Vance <coletain@gmail.com>
2020-09-30 10:19:43 -0700

    Adds configuration to silently disable

--- noback.js
+++ noback.js
@@ -3,6 +3,7 @@ let noback = true;
 window.addEventListener("popstate", async () => {
   if (noback) {
     history.pushState(null, null, '/game');
+    if (!game.settings.get('noback', 'prompt')) return;
     if (confirm(game.i18n.localize('NOBACK.confirm'))) {
       noback = false;
       history.back();
@@ -10,3 +11,14 @@ window.addEventListener("popstate", async () => {
     }
   }
 });
+
+Hooks.on('init', () => {
+  game.settings.register('noback', 'prompt', {
+    name: game.i18n.localize('NOBACK.prompt'),
+    hint: game.i18n.localize('NOBACK.promptHint'),
+    scope: 'client',
+    config: true,
+    type: Boolean,
+    default: true,
+  });
+})
